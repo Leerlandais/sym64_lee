2a0ad55cad515195c2d66b0244ad67026d88e58b
leerlandais <leerlandais@gmail.com>
2024-10-28 11:02:53 +0100

    started Fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,17 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+// load my entities
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Section;
+
+// load other dependencies
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory AS Faker;
+use Cocur\Slugify\Slugify;
+use DateTime;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void

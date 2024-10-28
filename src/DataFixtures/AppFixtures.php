d9358061493a8e9c93a170a90d54697d0686d921
leerlandais <leerlandais@gmail.com>
2024-10-28 11:25:41 +0100

    added users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -63,6 +63,40 @@ class AppFixtures extends Fixture
 
         $manager->persist($admin);
 
+
+        for($i = 3; $i < 8; $i++){
+            // create the 5 Editors
+            $redac = new User();
+            $redac->setUsername('redac'.$i);
+            $redac->setRoles(['ROLE_REDAC', 'ROLE_USER']);
+            $redac->setEmail('redac'.$i.'@redac.com');
+            $redac->setFullName('Redac '.$i);
+            $redac->setActivate(true);
+            $redac->setUniqid(uniqid('user_', true));
+            $pwdHash = $this->hasher->hashPassword($redac, 'redac'.$i);
+            $redac->setPassword($pwdHash);
+
+            $this->admins[$i] = $redac;
+            $manager->persist($redac);
+            // Gotta love PHPStorm - Other than typing 'for', I let it autofill the rest :-D
+        }
+
+        for($i = 1; $i < 25; $i++){
+            // create the users
+            $user = new User();
+            $user->setUsername('user'.$i);
+            $user->setRoles(['ROLE_USER']);
+            $user->setEmail($this->faker->email);
+            $user->setFullName($this->faker->name());
+            $user->setActivate(mt_rand(0,3)); // tidier way to get 1 false in 4, no need for an extra variable
+            $user->setUniqid(uniqid('user_', true));
+            $pwdHash = $this->hasher->hashPassword($user, 'user'.$i);
+            $user->setPassword($pwdHash);
+
+            $users[] = $user;
+            $manager->persist($user);
+        }
+
         $manager->flush();
     }
 }
